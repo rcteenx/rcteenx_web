d163bcedf037c863fcd3fd97448ff11c0df60dc4
Reyhan Çetin <reyhancetin@Reyhan-Mac-mini.local>
2023-10-19 14:15:35 +0300

    remove bootstrap package

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,4 @@
 import "./globals.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
 
 import { Inter } from "next/font/google";
 import Script from "next/script";
@@ -35,7 +33,6 @@ export default function RootLayout({ children }) {
           </div>
         </div>
         <Footer />
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" />
       </body>
     </html>
   );
